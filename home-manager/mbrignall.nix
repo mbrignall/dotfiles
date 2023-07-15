de23662ec57c4a6267ca2169c981827aee05f752
mbrignall <martin.brignall@ovo.com>
2023-07-15 09:07:57 +0100

    amended flake

--- home-manager/mbrignall.nix
+++ home-manager/mbrignall.nix
@@ -1,121 +1,24 @@
-{ config, pkgs, ... }:
-
 {
-  home = {
-    stateVersion = "23.05";
-    username = "mbrignall";
-    homeDirectory = "/home/mbrignall";
+  description = "NixOS configuration";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.emacs-overlay.url = "github:nix-community/emacs-overlay";
+
+  outputs = { self, nixpkgs, home-manager, emacs-overlay }: {
+    nixosConfigurations = {
+      mbrignixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          builtins.readFile
+          "/etc/nixos/hardware-configuration.nix"
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.users.mbrignall = import ./home-manager/mbrignall.nix;
+          }
+        ];
+      };
+    };
   };
-
-  # programs.zsh = {
-  #   enable = true;
-  #   ohMyZsh.enable = true;
-  #   ohMyZsh.theme = "powerlevel10k/powerlevel10k";
-  #   ohMyZsh.plugins = [ "git" "docker" "nix" ];
-  # };
-
-  # Home Manager allows you to manage user-specific files
-  # Here's an example that creates a symbolic link from the file
-  # /home/mbrignall/.config to the file .config in your repository
-  home.file.".config".source = ../.config;
-
-  # Here's how you can set a shell option
-  # programs.zsh.interactiveShellInit = ''
-  #   # Example shell option
-  #   setopt AUTO_CD
-  # '';
-
-  # Set packages
-  home.packages = with pkgs; [
-    alacritty
-    bat
-    bc
-    bfcal
-    blueberry
-    brightnessctl
-    clang
-    cmake
-    coreutils
-    direnv
-    docker
-    docker-compose
-    editorconfig-core-c
-    emacs
-    emptty
-    eww-wayland
-    fd
-    font-awesome_5
-    fuzzel
-    gcc
-    gsettings-desktop-schemas
-    git
-    gimp
-    glib
-    gnome3.adwaita-icon-theme
-    google-chrome
-    graphviz
-    grim
-    gvfs
-    python311Packages.grip
-    gnumake
-    hugo
-    imagemagick
-    ispell
-    jdk17
-    jq
-    nodePackages.js-beautify
-    karlender
-    libnotify
-    libtool
-    mako
-    maim
-    networkmanager-fortisslvpn
-    ntfs3g
-    exfat
-    nixfmt
-    nodejs
-    oh-my-zsh
-    pkgs.openai
-    pandoc
-    pavucontrol
-    plantuml
-    powerstat
-    python39
-    python39Packages.black
-    python39Packages.pyflakes
-    python39Packages.isort
-    pipenv
-    python39Packages.nose
-    python39Packages.pytest
-    python311Packages.weasyprint
-    qemu
-    ripgrep
-    rnix-lsp
-    shellcheck
-    shfmt
-    slack
-    slurp
-    nodePackages.stylelint
-    swayidle
-    swaylock
-    terminus-nerdfont
-    terraform
-    html-tidy
-    udisks
-    usermount
-    vim
-    virt-manager
-    vlc
-    vulkan-loader
-    waybar
-    wayland
-    wl-clipboard
-    wdisplays
-    wget
-    xdg-utils
-    xfce.thunar
-    xfce.thunar-volman
-    yarn
-    zsh
-  ];
 }
