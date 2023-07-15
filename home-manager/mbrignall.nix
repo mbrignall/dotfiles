92517c824fc6e866226d4966cb7d14a56b93ca28
mbrignall <martin.brignall@ovo.com>
2023-07-15 01:30:41 +0100

    adjusting mbrignall.nix

--- home-manager/mbrignall.nix
+++ home-manager/mbrignall.nix
@@ -1,14 +1,7 @@
 {
-  programs.zsh = {
-    enable = true;
-    ohMyZsh = {
-      enable = true;
-      custom = "./home-manager/.oh-my-zsh";
-    };
-  };
 
   home.file = {
-    ".zshrc".source = "./home-manager/.zshrc";
+    ".zshrc".source = "./.zshrc";
     ".config".source = "./.config";
     ".config".target = ".config";
   };
