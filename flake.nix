dcc5abaae2f701a30565d97b9dd810e72e2000d0
mbrignall <martin.brignall@ovo.com>
2023-09-01 22:40:47 +0100

    updating

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
       # Available through 'nixos-rebuild --flake .#your-hostname'
       nixosConfigurations = {
         # FIXME replace with your hostname
-        nixos = nixpkgs.lib.nixosSystem {
+        mbrignall = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           modules = [
             # > Our main nixos configuration file <
